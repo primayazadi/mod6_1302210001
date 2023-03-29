d95f054fbd49d8d13abe433108bb2f06d4d0bd4e
Your Name <you@example.com>
2023-03-29 17:00:57 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ SayaTubeUser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod6_1302210001
+{
+    internal class SayaTubeUser
+    {
+        private int id;
+        private String username;
+        public List<SayaTubeVideo> uploadedVideos;
+        public SayaTubeUser(String username)
+        {
+            this.username = username;
+        }
+
+        public int getTotalVideoPlayCount()
+        {
+            int total = 0;
+            foreach(var video in uploadedVideos)
+            {
+                total = total + video.getPlayCount();
+                
+            }
+            return total;
+        }
+
+        public void AddVideo(SayaTubeVideo video) 
+        {
+            uploadedVideos.Add(video);
+        }
+
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine("user :" + this.username);
+            foreach (var video in uploadedVideos)
+            {
+                Console.WriteLine("Judul video :" + video.getTitle);
+            }
+
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod6_1302210001
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int playCount;
+
+        public SayaTubeVideo(int id, string tite, int playCount)
+        {
+            this.id = id;
+            this.title = tite;
+            this.playCount = playCount;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            Debug.Assert(playCount > 300 && playCount < 0, "Input Penambahan Playlist 100 Per Panggilan Method Nya");
+
+        }
+
+        public void PrintVIdeoDetails()
+        {
+            Console.WriteLine("id :" + this.id);
+            Console.WriteLine("title :" + this.title);
+            Console.WriteLine("playCount :" +this.playCount);
+        }
+
+        public int getPlayCount()
+        {
+            return this.playCount;
+        }
+
+        public String getTitle()
+        {
+            return this.title;
+        }
+
+       
+    }
+}
